8e6769a452688fda08b3cab5222bad07eb2b1153
j4m-proton <azizijumanne63@gmail.com>
2025-11-07 11:14:46 +0300

    fix static

--- gnmpro/gnmpro/settings.py
+++ gnmpro/gnmpro/settings.py
@@ -150,10 +150,11 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 
 # Static files
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_ENDPOINT_URL}/static/'
+
 
 # Media files
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_ENDPOINT_URL}/'
 
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
