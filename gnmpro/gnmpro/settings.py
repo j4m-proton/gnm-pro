400eacd6ebd1da8050f57d5bc50647d654ebb650
j4m-proton <azizijumanne63@gmail.com>
2025-11-07 10:07:33 +0300

    fix storage

--- gnmpro/gnmpro/settings.py
+++ gnmpro/gnmpro/settings.py
@@ -149,10 +149,11 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 
-
-
-STATICFILES_STORAGE = 'gnmpro.storages_backends.StaticStorage'
-DEFAULT_FILE_STORAGE = 'gnmpro.storages_backends.MediaStorage'
-
+# Static files
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_ENDPOINT_URL}/static/'
-MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_ENDPOINT_URL}/media/'
+
+# Media files
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.{AWS_S3_ENDPOINT_URL}/'
+
