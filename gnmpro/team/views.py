28b5e497ac219f8df6b0b1f0663230c75825c8be
j4m-proton <azizijumanne63@gmail.com>
2025-11-08 10:46:27 +0300

    our-team recap

--- gnmpro/team/views.py
+++ gnmpro/team/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
-
+from .models import HeadTeam,MidleTopTeam,TeamMember
 def team(request):
-    return render(request, "team/our-team.html")
+    topTeam = HeadTeam.objects.all()
+    middleTeam = MidleTopTeam.objects.all()
+    teamAction = TeamMember.objects.all()
+
+    context = {
+        'title':'Our Team | GNM',
+        'topTeam': topTeam,
+        'middleTeam': middleTeam,
+        'teamAction': teamAction
+    }
+    return render(request, "team/our-team.html",context)
