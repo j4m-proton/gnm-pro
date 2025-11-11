a5cac8a1921359b4d893a19d99ef4ea44a1694dc
j4m-proton <azizijumanne63@gmail.com>
2025-11-11 09:11:10 +0300

    fix drop us a line

--- gnmpro/contactus/urls.py
+++ gnmpro/contactus/urls.py
@@ -5,5 +5,5 @@ app_name = "contactus"
 
 urlpatterns = [
     path("contact-us/", views.contact, name="contact-us"),
-    
+    path("contact-us-message/", views.ajax_contact_form, name="drop-us-msg"),
 ]
--- gnmpro/contactus/views.py
+++ gnmpro/contactus/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.http import JsonResponse
 from .models import OfficeLocation,ContactInfo,ContactUs
 
 def contact(request):
@@ -6,22 +7,6 @@ def contact(request):
     contactsection = ContactInfo.objects.get()
     hqOffice = OfficeLocation.objects.filter(isHQ=True)
     
-    if request.method == 'POST':
-        full_name = request.GET['full_name']
-        email = request.GET['email']
-        phone = request.GET['phone']
-        subject = request.GET['subject']
-        message = request.GET['message']
-
-        contact = ContactUs.objects.create(
-            full_name=full_name,
-            email=email,
-            phone=phone,
-            subject=subject,
-            message=message,
-        )
-        
-        return redirect("home:quote_success") 
     
     context = {
         'hqOffice': hqOffice,
@@ -29,3 +14,27 @@ def contact(request):
         'contactsection': contactsection
     }
     return render(request, "contact-us/contact-us.html",context)
+
+def ajax_contact_form(request):
+    full_name = request.GET['full_name']
+    email = request.GET['email']
+    phone = request.GET['phone']
+    subject = request.GET['subject']
+    message = request.GET['message']
+
+    contact = ContactUs.objects.create(
+        full_name=full_name,
+        email=email,
+        phone=phone,
+        subject=subject,
+        message=message,
+    )
+
+    
+
+    data = {
+        "bool": True,
+        "message": "Message Sent Successfully"
+    }
+
+    return JsonResponse({"data":data})
